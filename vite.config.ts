4214c4289344ca5517ef7d89aa7b7e90ccebabfa
yangxiaobo <13121189733@163.com>
2022-09-19 22:13:11 +0800

    feat: 🚀 新增配置文件
    - 新增 eslint、prettier
    - 新增环境变量
    - 新增 husky、lint-stage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,77 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
+import { createHtmlPlugin } from "vite-plugin-html";
+import viteCompression from "vite-plugin-compression";
+import eslintPlugin from "vite-plugin-eslint";
+import { wrapperEnv } from "./src/utils/getEnv";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+	const env = loadEnv(mode, process.cwd());
+	const viteEnv = wrapperEnv(env);
+
+	return {
+		// alias config
+		resolve: {
+			alias: {
+				"~@": resolve(__dirname, "./src"),
+			},
+		},
+		// global css
+		css: {
+			preprocessorOptions: {
+				less: {
+					// modifyVars: {
+					// 	"primary-color": "#1DA57A",
+					// },
+					javascriptEnabled: true,
+					additionalData: `@import "@/styles/var.less";`,
+				},
+			},
+		},
+		// server config
+		server: {
+			host: "0.0.0.0", // 服务器主机名，如果允许外部访问，可设置为"0.0.0.0"
+			port: viteEnv.VITE_PORT,
+			open: viteEnv.VITE_OPEN,
+			cors: true,
+			// https: false,
+			// 代理跨域
+			proxy: {
+				"/api": {
+					target: "https://xxx",
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+				},
+			},
+		},
+		plugins: [
+			react(),
+			createHtmlPlugin({
+				inject: {
+					data: {
+						title: viteEnv.VITE_APP_TITLE,
+					},
+				},
+			}),
+			// * EsLint 报错信息显示在浏览器界面上
+			eslintPlugin(),
+			// * 是否生成包预览
+			viteEnv.VITE_REPORT && visualizer(),
+			// * gzip compress
+			viteEnv.VITE_BUILD_GZIP &&
+				viteCompression({
+					verbose: true,
+					disable: false,
+					threshold: 10240,
+					algorithm: "gzip",
+					ext: ".gz",
+				}),
+		],
+		esbuild: {
+			pure: viteEnv.VITE_DROP_CONSOLE ? ["console.log", "debugger"] : [],
+		},
+	};
+});
