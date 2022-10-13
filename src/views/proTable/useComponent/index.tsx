7a2b78cbdb41c9cd8d40cffa0157618f38011e04
yangxiaobo <13121189733@163.com>
2022-10-13 14:57:48 +0800

    feat: 🚀 新增 iconfont 组件

--- src/views/proTable/useComponent/index.tsx
+++ src/views/proTable/useComponent/index.tsx
@@ -1,5 +1,14 @@
+import IconFont from "~@/components/IconFont";
+
 const UseComponent = () => {
-	return <h1>useComponent</h1>;
+	return (
+		<h1>
+			useComponent
+			<div>
+				<IconFont type="icon-file_music" style={{ fontSize: "4rem" }} />
+			</div>
+		</h1>
+	);
 };
 
 export default UseComponent;
